fe78fdd5cc223ef5fd71297273ee3c7a61463dff
vaiiga <vaigaps945@gmail.com>
2026-04-22 11:53:28 +0530

    Create Reverselinkedlist.c

---
+++ Reverselinkedlist.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* reverse(struct Node* head) {
+    struct Node *prev = NULL, *curr = head, *next = NULL;
+
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node *head = NULL, *temp;
+
+   
+    head = (struct Node*)malloc(sizeof(struct Node));
+    head->data = 1;
+    head->next = (struct Node*)malloc(sizeof(struct Node));
+    head->next->data = 2;
+    head->next->next = (struct Node*)malloc(sizeof(struct Node));
+    head->next->next->data = 3;
+    head->next->next->next = NULL;
+
+    printf("Original: ");
+    printList(head);
+
+    head = reverse(head);
+
+    printf("Reversed: ");
+    printList(head);
+
+    return 0;
+}
